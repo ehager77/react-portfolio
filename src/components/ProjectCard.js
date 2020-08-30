dc35d3fc2281c3a3179fa721ac0ae96f2d548cb3
ehager77 <eric@isabel.io>
2020-08-30 19:58:18 -0400

    finalized project-card styling and made app responsive for all devices

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -9,9 +9,9 @@ class ProjectCard extends React.Component {
 	render() {
 		return (
 				<Card className="project-card">
-					<Card.Img className="project-hero" variant="top" src={require('../assets/h2grow.png')} />
+					<Card.Img className="project-hero grow" variant="top" src={require('../assets/h2grow.png')} />
 					<Card.Body className="project-card-body" >
-						<Card.Title className="project-title" >CardTitle</Card.Title>
+						<Card.Title className="project-title" >EightCap</Card.Title>
 						<Card.Text className="project-subtitle">
 							Some quick example text to build on the card title.
 						</Card.Text>
